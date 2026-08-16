e1c3dc86542cb5b7f5c4b453cf50157136255962
Muskan-delG <77332041+Muskan-delG@users.noreply.github.com>
2026-08-16 20:16:07 +0530

    Add: longest-happy-string - submission-0

---
+++ Data Structures & Algorithms/longest-happy-string/submission-0.java
@@ -0,0 +1,63 @@
+class Letter {
+    char ch;
+    int freq;
+
+    Letter(char ch, int freq) {
+        this.ch = ch;
+        this.freq = freq;
+    }
+}
+
+class Solution {
+    public String longestDiverseString(int a, int b, int c) {
+
+        PriorityQueue<Letter> pq =
+            new PriorityQueue<>((x, y) -> y.freq - x.freq);
+
+        if (a > 0) pq.offer(new Letter('a', a));
+        if (b > 0) pq.offer(new Letter('b', b));
+        if (c > 0) pq.offer(new Letter('c', c));
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!pq.isEmpty()) {
+
+            Letter first = pq.poll();
+
+            // Can't use first character because it would create 3 in a row
+            if (sb.length() >= 2 &&
+                sb.charAt(sb.length() - 1) == first.ch &&
+                sb.charAt(sb.length() - 2) == first.ch) {
+
+                // No alternative character available
+                if (pq.isEmpty()) {
+                    break;
+                }
+
+                Letter second = pq.poll();
+
+                sb.append(second.ch);
+                second.freq--;
+
+                if (second.freq > 0) {
+                    pq.offer(second);
+                }
+
+                // Put first character back
+                pq.offer(first);
+
+            } else {
+
+                // Safe to use first character
+                sb.append(first.ch);
+                first.freq--;
+
+                if (first.freq > 0) {
+                    pq.offer(first);
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
